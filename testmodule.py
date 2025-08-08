e35d41d52351a8c4a20e02d5722b6114b5d1ea66
Arogya Koirala <arogyakoirala@gmail.com>
2025-08-07 20:58:56 -0700

    Update with SR and correct Study Desc Logic

--- testmodule.py
+++ testmodule.py
@@ -52,7 +52,7 @@ def create_sr(file, in_folder, out_folder, series_uid, settings):
     ds.StudyInstanceUID = ref_ds.StudyInstanceUID
     ds.SeriesNumber = ref_ds.SeriesNumber + settings["series_offset"]
     ds.SeriesDescription = "SR Report"
-    ds.StudyDescription = ref_ds.StudyDescription + " OUTPUT" if hasattr(ref_ds, 'StudyDescription') else "OUTPUT"
+    ds.StudyDescription = ref_ds.StudyDescription + " OUTPUT SR" if hasattr(ref_ds, 'StudyDescription') else "OUTPUT SR"
     ds.Modality = "SR"
     ds.Manufacturer = "Mercure Test Module"
     
@@ -210,7 +210,7 @@ def process_image(file, in_folder, out_folder, series_uid, settings):
     ds.SeriesNumber = ds.SeriesNumber + settings["series_offset"]
     # Update the series description to indicate which the modified DICOM series is
     ds.SeriesDescription = "FILTER(" + ds.SeriesDescription + ")"
-    ds.StudyDescription = ds.StudyDescription + " OUTPUT" if hasattr(ds, 'StudyDescription') else "OUTPUT"
+    ds.StudyDescription = ds.StudyDescription + " OUTPUT FILTER" if hasattr(ds, 'StudyDescription') else "OUTPUT FILTER"
     # Access the pixel data of the input image, filter it, and store it
     pixels = ds.pixel_array
     blurred_pixels = gaussian_filter(pixels, sigma=settings["sigma"])
